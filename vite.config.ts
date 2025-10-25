a56826fdf6c29d4fd4aadc66c6e4161ca71307bb
konatenhamedm <konatenhamed@gmail.com>
2025-10-25 14:16:25 +0000

    savegarde

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 		  '/api': {
-			target: 'http://backend.leadagro.net',
+			target: 'https://backend.leadagro.net',
 			changeOrigin: true,
 			secure: false, // Désactiver si le certificat SSL est auto-signé
 			rewrite: (path) => path.replace(/^\/api/, '/api') // Garde le préfixe
